acdc82d41d1d4538308f0b0756ddd22bf9a5d263
stanislavk <542557+StanislavK@users.noreply.github.com>
2021-12-24 11:46:24 +0100

    Remove storyboards

--- NoStoryboards/ViewController.swift
+++ NoStoryboards/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  NoStoryboards
-//
-//  Created by Stanislav Kasprik on 24.12.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
